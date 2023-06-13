5287613bb5c5404b2335d68fc80d1ce8047ae2a2
Matheus Cunha <cunhamatheus.m4@gmail.com>
2023-06-13 11:27:24 -0300

    Design finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainInfoComponent } from './main-info/main-info.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainInfoComponent, SearchBoxComponent, DetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
